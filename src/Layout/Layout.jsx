a5be75a7918f3a6f5848b455dbd9e78a15b8f7aa
Manoj Belbase <manojbelbase56@gmail.com>
2024-11-14 20:23:43 +0545

    build error solve

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,6 @@
-import React, { useContext } from "react";
 import Navbar from "../Components/Shared/Navbar";
 import { Outlet } from "react-router-dom";
-import myContext from "../context/myContext";
-
 const Layout = () => {
-  const name = useContext(myContext);
-  console.log(name);
   return (
     <div className="flex flex-col items-center w-full">
       <div className="w-full max-w-screen-xl">
